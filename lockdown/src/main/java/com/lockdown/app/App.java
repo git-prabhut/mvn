e1c8f160e9f1700ed7227e9bcc8c7b251302cfd6
Prabu T <prabu.thiagaraj@wipro.com>
2020-07-05 11:25:06 +0000

    Initial commit

---
+++ lockdown/src/main/java/com/lockdown/app/App.java
@@ -0,0 +1,13 @@
+package com.lockdown.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
