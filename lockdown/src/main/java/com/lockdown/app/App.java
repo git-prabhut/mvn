3c9fb5b03fcebdb3c05f0330c30ed8ad54ab8ee6
Prabu T <pthiagaraj@gmail.com>
2020-12-01 10:22:32 +0000

    Changed from 150 to 1500

--- lockdown/src/main/java/com/lockdown/app/App.java
+++ lockdown/src/main/java/com/lockdown/app/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 	/* Added as part of MR65478 */
-	int number=150;
+	int number=2020;
         System.out.println( "Hello World!" );
         System.out.println( "Number = " + number );
     }
