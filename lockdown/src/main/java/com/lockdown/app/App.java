212df7b212bfc24f977382d91cbee0429feea667
Prabu T <pthiagaraj@gmail.com>
2020-12-01 09:54:48 +0000

    Modified for defect MR64378

--- lockdown/src/main/java/com/lockdown/app/App.java
+++ lockdown/src/main/java/com/lockdown/app/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+	/* Added as part of MR65478 */
+	int number=100;
         System.out.println( "Hello World!" );
+        System.out.println( "Number = " + number );
     }
 }
