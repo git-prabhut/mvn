7f16936e653225874ab08be7978c495ae1acd27c
Prabu T <prabu.thiagaraj@wipro.com>
2021-04-24 05:17:07 +0000

    Removed unwanted SOP - Prabu

--- lockdown/src/main/java/com/lockdown/app/App.java
+++ lockdown/src/main/java/com/lockdown/app/App.java
@@ -14,7 +14,5 @@ public class App
 	int number3=10;
         System.out.println( "Hello World!" );
         System.out.println( "Number = " + number + "Number2= " + number2);
-        System.out.println( "Mepco Demo");
-        System.out.println( "Mepco Demo2");
     }
 }
